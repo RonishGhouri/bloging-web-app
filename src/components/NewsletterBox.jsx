cd1570fd4deb1462c686b61d8c5afe577def6d96
RonishGhouri <ronishemanual974@gmail.com>
2025-04-08 02:13:58 +0500

    f

---
+++ src/components/NewsletterBox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NewsletterBox = () => {
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="text-center">
+      <p className="text-2xl font-medium text-gray-gray-800">
+        Subscribe now & get 20% off
+      </p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, in.</p>
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 border mx-auto my-6 pl-3 "
+      >
+        <input
+          className="w-full sm:flex-1 outline-none"
+          type="email"
+          placeholder="Enter your email"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-black text-white text-xs py-4 px-10"
+        >
+          SUBSCRIBE
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewsletterBox;
